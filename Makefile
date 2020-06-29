69eb614a1b32a2c7791d3963a3c4829a9937d774
mishana <mishana4life@gmail.com>
2020-06-29 16:56:56 +0300

    optimized prints by "loop-unrolling"

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CC = g++
 # We use optimization level 1 to avoid automatic vectorization with both GCC
 # and Clang. GCC versions 4 and 5 will do automatic vectorization only at
 # optimization level 3. Clang apparently will do it at level 2 or 3.
-CFLAGS =  -O3 -funroll-loops
+CFLAGS =  -Ofast -march=native
 ASFLAGS = -g
 
 
@@ -16,7 +16,14 @@ DIST_FILES =  \
 all: euler
 
 euler: euler.cpp
-	$(CC) -std=c++11 $(CFLAGS) -o $@ $^
+	$(CC) -std=c++11 $(CFLAGS) -fprofile-generate=prof.data -o $@ $^
+	./$@ 1500000 > /dev/null
+	$(CC) -std=c++11 $(CFLAGS) -fprofile-use=prof.data -o $@ $^
+
+time:
+	python -m timeit -s 'import os' 'os.system("./euler 150 > out.txt")'
+	python -m timeit -s 'import os' 'os.system("./euler 15000 > out.txt")'
+	python -m timeit -s 'import os' 'os.system("./euler 1500000 > out.txt")'
 
 clean:
 	rm -f *.o
--- euler.cpp
+++ euler.cpp
@@ -1,86 +1,13 @@
 #include <iostream>
 #include <vector>
 
+#define MAX 1500000
 
-unsigned int gcd(unsigned int u, unsigned int v) {
-    unsigned int shift = 0;
-
-    /* GCD(0,v) == v; GCD(u,0) == u, GCD(0,0) == 0 */
-    if (u == 0) return v;
-    if (v == 0) return u;
-
-    /* Let shift := lg K, where K is the greatest power of 2
-        dividing both u and v. */
-    while (((u | v) & 1) == 0) {
-        shift++;
-        u >>= 1;
-        v >>= 1;
-    }
-
-    while ((u & 1) == 0)
-        u >>= 1;
-
-    /* From here on, u is always odd. */
-    do {
-        /* remove all factors of 2 in v -- they are not common */
-        /*   note: v is not zero, so while will terminate */
-        while ((v & 1) == 0)
-            v >>= 1;
-
-        /* Now u and v are both odd. Swap if necessary so u <= v,
-            then set v = v - u (which is even). For bignums, the
-             swapping is just pointer movement, and the subtraction
-              can be done in-place. */
-        if (u > v) {
-            unsigned int t = v;
-            v = u;
-            u = t; // Swap u and v.
-        }
-
-        v -= u; // Here v >= u.
-    } while (v != 0);
-
-    /* restore common factors of 2 */
-    return u << shift;
-}
+unsigned int phi[MAX + 1] = {0};
 
 
-unsigned int phi(unsigned int n) {
-    unsigned int result = n; // Initialize result as n
-
-    if (n % 2 == 0) {
-
-        // If yes, then update n and result
-        while (n % 2 == 0)
-            n /= 2;
-        result -= result / 2;
-    }
-    // Consider all prime factors of n and for every prime
-    // factor p, multiply result with (1 - 1/p)
-    for (unsigned int p = 3; p * p <= n; p += 2) {
-        if (n == 1)
-            return result;
-        // Check if p is a prime factor.
-        if (n % p == 0) {
-
-            // If yes, then update n and result
-            while (n % p == 0)
-                n /= p;
-            result -= result / p;
-        }
-    }
-
-    // If n has a prime factor greater than sqrt(n)
-    // (There can be at-most one such prime factor)
-    if (n > 1)
-        result -= result / n;
-
-    return result;
-}
-
-
-void phi_1_to_n(unsigned int n) {
-    std::vector<unsigned int> phi(n + 1);
+void phi_1_to_n(int n) {
+//    std::vector<unsigned int> phi(n + 1);
     phi[0] = 0;
     phi[1] = 1;
 
@@ -88,20 +15,48 @@ void phi_1_to_n(unsigned int n) {
         if (!phi[i]) {
             phi[i] = i - 1;
             for (int j = i << 1; j <= n; j += i) {
-                if (!phi[j]) phi[j] = j;
+//                if (!phi[j]) phi[j] = j;
+                phi[j] = (phi[j] == 0) * j + phi[j];
+
                 phi[j] -= phi[j] / i;
             }
         }
     }
 
-    for (int i = 1; i < n; i++)
-        printf("%d ", phi[i]);
-    printf("%d", phi[n]);
+    printf("%d", phi[1]);
+    int i = 2;
+    // "loop-unroll" the printf calls
+    for (; i < n - 63; i += 64)
+        printf(" %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d"
+               " %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+               phi[i], phi[i + 1], phi[i + 2], phi[i + 3], phi[i + 4], phi[i + 5], phi[i + 6], phi[i + 7],
+               phi[i + 8], phi[i + 9], phi[i + 10], phi[i + 11], phi[i + 12], phi[i + 13], phi[i + 14], phi[i + 15],
+               phi[i + 16], phi[i + 17], phi[i + 18], phi[i + 19], phi[i + 20], phi[i + 21], phi[i + 22], phi[i + 23],
+               phi[i + 24], phi[i + 25], phi[i + 26], phi[i + 27], phi[i + 28], phi[i + 29], phi[i + 30], phi[i + 31],
+               phi[i + 32], phi[i + 33], phi[i + 34], phi[i + 35], phi[i + 36], phi[i + 37], phi[i + 38], phi[i + 39],
+               phi[i + 40], phi[i + 41], phi[i + 42], phi[i + 43], phi[i + 44], phi[i + 45], phi[i + 46], phi[i + 47],
+               phi[i + 48], phi[i + 49], phi[i + 50], phi[i + 51], phi[i + 52], phi[i + 53], phi[i + 54], phi[i + 55],
+               phi[i + 56], phi[i + 57], phi[i + 58], phi[i + 59], phi[i + 60], phi[i + 61], phi[i + 62], phi[i + 63]);
+    for (; i < n - 31; i += 32)
+        printf(" %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+               phi[i], phi[i + 1], phi[i + 2], phi[i + 3], phi[i + 4], phi[i + 5], phi[i + 6], phi[i + 7],
+               phi[i + 8], phi[i + 9], phi[i + 10], phi[i + 11], phi[i + 12], phi[i + 13], phi[i + 14], phi[i + 15],
+               phi[i + 16], phi[i + 17], phi[i + 18], phi[i + 19], phi[i + 20], phi[i + 21], phi[i + 22], phi[i + 23],
+               phi[i + 24], phi[i + 25], phi[i + 26], phi[i + 27], phi[i + 28], phi[i + 29], phi[i + 30], phi[i + 31]);
+    for (; i < n - 15; i += 16)
+        printf(" %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+               phi[i], phi[i + 1], phi[i + 2], phi[i + 3], phi[i + 4], phi[i + 5], phi[i + 6], phi[i + 7],
+               phi[i + 8], phi[i + 9], phi[i + 10], phi[i + 11], phi[i + 12], phi[i + 13], phi[i + 14], phi[i + 15]);
+    for (; i < n - 7; i += 8)
+        printf(" %d %d %d %d %d %d %d %d",
+               phi[i], phi[i + 1], phi[i + 2], phi[i + 3], phi[i + 4], phi[i + 5], phi[i + 6], phi[i + 7]);
+    for (; i < n + 1; ++i)
+        printf(" %d", phi[i]);
 }
 
 
 int main(int argc, char *argv[]) {
-    unsigned int num = std::stoi(argv[1]);
+    int num = std::stoi(argv[1]);
     phi_1_to_n(num);
 
     return 0;
